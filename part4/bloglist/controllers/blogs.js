4ca33f8518374b9f313c997005c0c01c839b5378
Artt2 <arttutjpesonen@gmail.com>
2023-06-20 16:31:40 +0300

    exercises 4.15-4.16

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -7,13 +7,6 @@ blogsRouter.get('/', async (request, response) => {
 })
 
 blogsRouter.post('/', async (request, response) => {
-  /*
-  const blog = new Blog(request.body)
-
-  const savedBlog = await blog.save()
-  response.status(201).json(savedBlog)
-  */
-
   const {title, author, url, likes} = request.body
 
   const blog = new Blog({
