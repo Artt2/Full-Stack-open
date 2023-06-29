61af9ea1bbad9397eb07228f683a0aed1448aa31
Artt2 <arttutjpesonen@gmail.com>
2023-06-29 15:48:14 +0300

    part 6 exercises 6.3-6.8

---
+++ part6/redux-anecdotes/src/App.js
@@ -0,0 +1,25 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { voteAnecdote, createAnecdote } from './reducers/anecdoteReducer'
+import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
+
+const App = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    console.log('vote', id)
+    //use dispatch and imported vote function from anecdoteReducer
+    dispatch(voteAnecdote(id))
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      <AnecdoteList />
+      <AnecdoteForm />
+    </div>
+  )
+}
+
+export default App
